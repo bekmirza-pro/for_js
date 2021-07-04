73a5bf438d82ad6df0a0147a42cb7e635964b613
bekmirza-pro <bekmirzaprogrammer@yandex.ru>
2021-07-04 10:51:26 -0700

    First commit js

---
+++ js/main.js
@@ -0,0 +1,34 @@
+ var firstName = prompt('Ismingizni kiriting : ', 'Bekmirza');
+ var secondName = prompt('Familiyangizni kiriting : ', 'Yoqubov');
+ var studyName = prompt('O"quv muassasangizni kiriting : ', 'TATU');
+ var faculty = prompt('Fakultetingizni kiriting : ', 'Axborot xavfsizligi');
+ var age = prompt('Yoshingizni kiriting : ', '19');
+ var addres = prompt('Manzilingizni kiriting : ', 'Namangan');
+ var profession = prompt('Kasbingizni kiriting : ', 'Web Developer');
+ var hobby = prompt('Sevimli mashg"ulotingizni kiriting : ', 'Footbal');
+ var youYear = prompt('Yilingizni kiriting : ', '2002');
+ var date = prompt('Qaysi sanada tug"ilgansiz : ', '9-sentabr');
+
+
+ console.log(
+     'Ism : ' +
+     firstName +
+     '\nFamiliyangiz : ' +
+     secondName +
+     "\nO'quv muassasa : " +
+     studyName +
+     "\nFakultet : " +
+     faculty +
+     "\nYoshingiz : " +
+     age +
+     "\nManzilingiz : " +
+     addres +
+     "\nKasbingiz : " +
+     profession +
+     "\nSevimli mashg'ulotingiz : " +
+     hobby +
+     "\nYilingiz : " +
+     youYear +
+     "\nTug'ilgan sanangiz : " +
+     date 
+ )
