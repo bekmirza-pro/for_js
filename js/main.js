73489db6d8e72e4513d7a3ebff18b22de1d11df4
bekmirza-pro <bekmirzaprogrammer@yandex.ru>
2021-07-07 16:11:27 -0700

    if and else commit to js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,61 @@
- var firstName = prompt('Ismingizni kiriting : ', 'Bekmirza');
- var secondName = prompt('Familiyangizni kiriting : ', 'Yoqubov');
- var studyName = prompt('O"quv muassasangizni kiriting : ', 'TATU');
- var faculty = prompt('Fakultetingizni kiriting : ', 'Axborot xavfsizligi');
- var age = prompt('Yoshingizni kiriting : ', '19');
- var addres = prompt('Manzilingizni kiriting : ', 'Namangan');
- var profession = prompt('Kasbingizni kiriting : ', 'Web Developer');
- var hobby = prompt('Sevimli mashg"ulotingizni kiriting : ', 'Footbal');
- var youYear = prompt('Yilingizni kiriting : ', '2002');
- var date = prompt('Qaysi sanada tug"ilgansiz : ', '9-sentabr');
+//  var firstName = prompt('Ismingizni kiriting : ', 'Bekmirza');
+//  var secondName = prompt('Familiyangizni kiriting : ', 'Yoqubov');
+//  var studyName = prompt('O"quv muassasangizni kiriting : ', 'TATU');
+//  var faculty = prompt('Fakultetingizni kiriting : ', 'Axborot xavfsizligi');
+//  var age = prompt('Yoshingizni kiriting : ', '19');
+//  var addres = prompt('Manzilingizni kiriting : ', 'Namangan');
+//  var profession = prompt('Kasbingizni kiriting : ', 'Web Developer');
+//  var hobby = prompt('Sevimli mashg"ulotingizni kiriting : ', 'Footbal');
+//  var youYear = prompt('Yilingizni kiriting : ', '2002');
+//  var date = prompt('Qaysi sanada tug"ilgansiz : ', '9-sentabr');
 
 
- console.log(
-     'Ism : ' +
-     firstName +
-     '\nFamiliyangiz : ' +
-     secondName +
-     "\nO'quv muassasa : " +
-     studyName +
-     "\nFakultet : " +
-     faculty +
-     "\nYoshingiz : " +
-     age +
-     "\nManzilingiz : " +
-     addres +
-     "\nKasbingiz : " +
-     profession +
-     "\nSevimli mashg'ulotingiz : " +
-     hobby +
-     "\nYilingiz : " +
-     youYear +
-     "\nTug'ilgan sanangiz : " +
-     date 
- )
+//  console.log(
+//      'Ism : ' +
+//      firstName +
+//      '\nFamiliyangiz : ' +
+//      secondName +
+//      "\nO'quv muassasa : " +
+//      studyName +
+//      "\nFakultet : " +
+//      faculty +
+//      "\nYoshingiz : " +
+//      age +
+//      "\nManzilingiz : " +
+//      addres +
+//      "\nKasbingiz : " +
+//      profession +
+//      "\nSevimli mashg'ulotingiz : " +
+//      hobby +
+//      "\nYilingiz : " +
+//      youYear +
+//      "\nTug'ilgan sanangiz : " +
+//      date 
+//  )
+
+// var a = prompt('Dastlabki sonni kiriting :');
+// var b = prompt('Keyingi sonni kiriting :');
+// // var c = prompt('Keyingi sonni kiriting :');
+
+// var c = a + b ;
+
+// if(c = a + b){
+//     console.log('Ha teng')
+// }
+// else{
+//     console.log('yoq teng emas')
+// }
+
+var elHeader = document.querySelector('.heading_white')
+
+
+
+alert('Sayohat uchun xarajatlar :  Borish-kelish samolyot bileti - $500  , - Mehmonxona to’lovi (to’liq safar davomiyligi uchun) - $250 , - Muzey va ko’ngilochar joylar uchun - 120 yevro')
+alert('Hozirgi pul kurslari : $1 = 9433.34 so’m , 1 yevro = 10354.03 so’m')
+var a = prompt('O"zingizdagi mavjud mablag"ni kiriting :');
+
+if(a >= 750*9433.34 + 120*10354.03){
+    console.log(elHeader.textContent = 'Oq yo"l Alisher');
+} else{ 
+    console.log(elHeader.textContent = 'Alisher, ozgina sabr qilish kerak bo’lar ekan.');
+}
